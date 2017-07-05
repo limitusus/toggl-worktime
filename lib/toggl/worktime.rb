6d53b4d003f551a4ba0c345dffbf3eb41069b2f6
limitusus <limit.usus@gmail.com>
2017-07-06 00:08:59 +0900

    Toggl-worktime driver

--- lib/toggl/worktime.rb
+++ lib/toggl/worktime.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'toggl/worktime/version'
 require 'toggl/worktime/merger'
 require 'toggl/worktime/time'
+require 'toggl/worktime/driver'
 
 module Toggl
   # Main module
---
+++ lib/toggl/worktime/driver.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Toggl
+  module Worktime
+    # Toggle API driver
+    class Driver
+      attr_reader :toggl
+
+      def initialize
+        @toggl = TogglV8::API.new
+        @merger = nil
+        @work_time = nil
+      end
+
+      def time_entries(month, day, hour, timezone)
+        now = DateTime.now
+        offset = Toggl::Worktime::Time.zone_offset(timezone)
+        beginning_day = DateTime.new(now.year, month, day, hour, 0, 0, offset)
+        ending_day = beginning_day + 1
+        toggl.get_time_entries(start_date: beginning_day.iso8601, end_date: ending_day.iso8601)
+      end
+
+      def me
+        @toggl.me(true)
+      end
+
+      def merge!(month, day, hour, timezone)
+        time_entries = time_entries(month, day, hour, timezone)
+        @merger = Toggl::Worktime::Merger.new(time_entries)
+        @work_time = @merger.merge
+      end
+
+      def write
+        @work_time.each do |span|
+          begin_s = time_expr(span[0])
+          end_s = time_expr(span[1])
+          puts "#{begin_s} - #{end_s}"
+        end
+      end
+
+      def time_expr(t)
+        t ? t.strftime('%F %T') : 'nil'
+      end
+    end
+  end
+end
