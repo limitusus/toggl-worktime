b3562bc2d65611bb50faeae807558bbd14b4ead5
Tomoya Kabe <limit.usus@gmail.com>
2018-08-01 11:43:45 +0900

    Add feature to specify year

--- lib/toggl/worktime/driver.rb
+++ lib/toggl/worktime/driver.rb
@@ -14,10 +14,9 @@ module Toggl
         @work_time = nil
       end
 
-      def time_entries(month, day, hour, timezone)
-        now = DateTime.now
+      def time_entries(year, month, day, hour, timezone)
         offset = Toggl::Worktime::Time.zone_offset(timezone)
-        beginning_day = DateTime.new(now.year, month, day, hour, 0, 0, offset)
+        beginning_day = DateTime.new(year, month, day, hour, 0, 0, offset)
         ending_day = beginning_day + 1
         toggl.get_time_entries(start_date: beginning_day.iso8601, end_date: ending_day.iso8601)
       end
@@ -26,8 +25,8 @@ module Toggl
         @toggl.me(true)
       end
 
-      def merge!(month, day, hour, timezone)
-        time_entries = time_entries(month, day, hour, timezone)
+      def merge!(year, month, day, hour, timezone)
+        time_entries = time_entries(year, month, day, hour, timezone)
         @merger = Toggl::Worktime::Merger.new(time_entries, timezone, @max_working_interval)
         @work_time = @merger.merge
       end
